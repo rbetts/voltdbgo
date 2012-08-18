fc8e1e43d6e10eb6dc897d41aec42c3ea49229ea
Ryan Betts <rbetts@gmail.com>
2012-08-18 13:45:02 -0400

    A more complete voter example.

--- example/voter.go
+++ example/voter.go
@@ -1,44 +1,136 @@
+// An example using the voltdb/examples/voter application.
+// To run this, first start the voltdb voter example:
+// `cd voltdb-x.y.z/example/voter && ./run.sh`.
+// Then run this go program.
 package main
 
 import (
 	"fmt"
 	"github.com/rbetts/voltdbgo/voltdb"
 	"log"
+	"math/rand"
+    "os"
+	"time"
+    "runtime/pprof"
 )
 
-// A simple example using the voltdb/examples/voter application.
+var ttlContestants = 6
+var contestants = "Andy,Bob,Cathy,Doug,Erik,Greg"
+var voterGoroutines = 8
+var votingDuration time.Duration = 100 * time.Second
 
-type ResultsRow struct {
-	Contestant string
-	Id         int
-	Votes      int
-}
-
-func (row *ResultsRow) GoString() string {
-	return fmt.Sprintf("%v\t%v\t%v\n", row.Contestant, row.Id, row.Votes)
+// ScalarResult is used to read scalar stored procedures results.
+type ScalarResult struct {
+	Result int
 }
 
 func main() {
 	volt, err := voltdb.NewConnection("username", "", "localhost:21212")
+	defer volt.Close()
 	if err != nil {
 		log.Fatalf("Connection error %v\n", err)
 	}
 	if !volt.TestConnection() {
 		log.Fatalf("Connection error: failed to ping VoltDB database.")
 	}
+
+    f, err := os.Create("/Users/rbetts/voterprof")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	initialize(volt)
+    pprof.StartCPUProfile(f)
+    vote();
+    pprof.StopCPUProfile()
 	printResults(volt)
-	volt.Close()
 }
 
+// Add contestants to the database if necessary
+func initialize(volt *voltdb.Conn) {
+	rsp, err := volt.Call("Initialize", ttlContestants, contestants)
+	if err != nil {
+		log.Fatalf("Failed in initialize database. %v\n", err)
+	}
+	if rsp.Status() == voltdb.SUCCESS {
+		var rs ScalarResult
+		rsp.Table(0).Next(&rs)
+		fmt.Printf("Initialized %d contestants.\n", rs.Result)
+	} else {
+		log.Fatalf("Failed to initialize %#v.\n", rsp)
+    }
+}
+
+// vote starts voterGoroutine number votes loops and returns when
+// they have all run to conclusion.
+func vote() {
+	var joiners = make([]chan int, 0)
+	for i := 0; i < voterGoroutines; i++ {
+		var joinchan = make(chan int)
+		joiners = append(joiners, joinchan)
+		go placeVotes(joinchan)
+	}
+    var totalVotes = 0
+	for v, join := range joiners {
+		votes := <-join
+        totalVotes += votes
+		fmt.Printf("Voter %v finished and placed %v votes.\n", v, votes)
+	}
+    fmt.Printf("Generated %v votes in %v seconds (%0.0f votes/second)\n",
+        totalVotes, votingDuration.Seconds(),
+        float64(totalVotes)/votingDuration.Seconds())
+	return
+}
+
+// placeVotes votes for votingDuration seconds.
+func placeVotes(join chan int) {
+	volt, err := voltdb.NewConnection("username", "", "localhost:21212")
+	defer volt.Close()
+	if err != nil {
+		log.Fatalf("Connection error db. %v\n", err)
+	}
+
+	timeout := time.After(votingDuration)
+	placedVotes := 0
+
+	for {
+		select {
+		case <-timeout:
+			join <- placedVotes
+			return
+		default:
+			// construct a phoneNumber with a valid area code.
+			var phoneNumber int64 = (5080000000 + int64(rand.Int31()))
+			// pick a contestant at random (contestant ids are 1-based)
+			var contestant = (rand.Int() % ttlContestants) + 1
+			rsp, err := volt.Call("Vote", phoneNumber, contestant, 100)
+			if err != nil {
+				log.Fatalf("Error voting. %v\n", err)
+			}
+			if rsp.Status() == voltdb.SUCCESS {
+                placedVotes++
+			} else {
+				fmt.Printf("Vote failed %#v\n", rsp)
+			}
+		}
+	}
+}
+
+// printResults displays the current vote tally for each contestant.
 func printResults(volt *voltdb.Conn) {
+    type ResultsRow struct {
+        Contestant string
+        Id         int
+        Votes      int
+    }
 	var row ResultsRow
+    fmt.Printf("\nCurrent results:\n")
 	response, _ := volt.Call("Results")
 	table := response.Table(0)
-
 	for table.HasNext() {
 		if err := table.Next(&row); err != nil {
 			log.Fatalf("Table iteration error %v\n", err)
 		}
-		fmt.Printf("%#v", &row)
+        fmt.Printf("%v\t%v\t%v\n", row.Contestant, row.Id, row.Votes)
 	}
 }
